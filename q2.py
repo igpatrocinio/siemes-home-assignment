2321f74936a97c0c0370c72cb8ba005d9376f8bf
Igor Patrocinio <105254279+igpatrocinio@users.noreply.github.com>
2025-03-11 14:33:10 -0300

    Update q2.py

--- q2.py
+++ q2.py
@@ -8,36 +8,45 @@ import pandas as pd
 
 def main():
 
-    with open('records.json', 'r') as f:
-        contents = f.read()
+    try:
+        with open('records.json', 'r') as f:
+            contents = f.read()
+        try:
+            # reading the contents as a json object
+            json_contents = json.loads(contents)
+        except json.JSONDecodeError:
+            print("JSON file is not able to be properly read. Please fix the malformed strings and try it again")
 
     # reading the contents as a json object
-    json_contents = json.loads(contents)
+        json_contents = json.loads(contents)
 
-    # converting the json to a dataframe
-    df = pd.DataFrame(json_contents)
+        # converting the json to a dataframe
+        df = pd.DataFrame(json_contents)
 
-    # converting the last_login to a datetime object
-    df["last_login"] = pd.to_datetime(df["last_login"])
+        # converting the last_login to a datetime object
+        df["last_login"] = pd.to_datetime(df["last_login"])
 
-    # sorting the df by the time_login column
-    sorted_df = df.sort_values(by="last_login").reset_index(drop=True)
+        # sorting the df by the time_login column
+        sorted_df = df.sort_values(by="last_login").reset_index(drop=True)
 
-    # creating the lagged last login and user
-    sorted_df["previous_last_user"] = sorted_df["id"].shift(1)
-    sorted_df["previous_last_login"] = sorted_df["last_login"].shift(1)
+        # creating the lagged last login and user
+        sorted_df["previous_last_user"] = sorted_df["id"].shift(1)
+        sorted_df["previous_last_login"] = sorted_df["last_login"].shift(1)
 
-    # creating difference column, between the last user login and the previous one
-    sorted_df["diff"] = sorted_df["previous_last_login"] - sorted_df["last_login"]
-    sorted_df["diff"] = abs(sorted_df["diff"].dt.total_seconds())
+        # creating difference column, between the last user login and the previous one
+        sorted_df["diff"] = sorted_df["previous_last_login"] - sorted_df["last_login"]
+        sorted_df["diff"] = abs(sorted_df["diff"].dt.total_seconds())
 
-    # getting the smallest difference between the current user and previous one
-    final_df = sorted_df.sort_values(by="diff").head(1)
+        # getting the smallest difference between the current user and previous one
+        final_df = sorted_df.sort_values(by="diff").head(1)
 
-    # renaming the columns for a friendlier name
-    final_df.rename(columns={'id': 'user1', 'previous_last_user': 'user2'}, inplace=True)
+        # renaming the columns for a friendlier name
+        final_df.rename(columns={'id': 'user1', 'previous_last_user': 'user2'}, inplace=True)
 
-    print(f"The 2 users who logged in at the closest time to each other:\n{final_df[['user1', 'user2']].to_string(index=False)}")
+        print(f"The 2 users who logged in at the closest time to each other:\n{final_df[['user1', 'user2']].to_string(index=False)}")
+
+    except FileNotFoundError:
+        print("The file was not found. Please add it or rename it.")
 
 
 if __name__ == '__main__':
